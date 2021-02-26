2de3ac7295428f1c7a5ab32417e4b0789c91ea68
Luiz <luizamorimsousa@hotmail.com>
2021-02-26 12:54:59 -0300

    second commit arq

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,65 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import { CountdownContext } from "../contexts/CountdownContext";
+
+import styles from "../styles/components/ChallengeBox.module.css";
+
+export function ChallengeBox() {
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(
+    ChallengesContext
+  );
+  const { resetCountdown } = useContext(CountdownContext);
+
+  function handChallengeSucceeded() {
+    completeChallenge();
+    resetCountdown();
+  }
+
+  function handChallengeFailed() {
+    resetChallenge();
+    resetCountdown();
+  }
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {activeChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe {activeChallenge.amount} xp</header>
+
+          <main>
+            <img src={`icons/${activeChallenge.type}.svg`} />
+            <strong>Novo desafio</strong>
+            <p>{activeChallenge.description}</p>
+          </main>
+
+          <footer>
+            <button
+              type="button"
+              className={styles.challengeFailedButton}
+              onClick={handChallengeFailed}
+            >
+              Falhei
+            </button>
+            <button
+              type="button"
+              className={styles.challengeSucceededButton}
+              onClick={handChallengeSucceeded}
+            >
+              Completei
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>
+            Finalize um ciclo para receber um desafio.
+            <p>
+              <img src="icons/level-up.svg" alt="Level Up" />
+              Avance de level completando desafios.
+            </p>
+          </strong>
+        </div>
+      )}
+    </div>
+  );
+}
